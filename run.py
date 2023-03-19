b62fb0c64dbf969ffe7c472d0f05dfd65dd028c9
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-03-18 18:08:10 -0600

    Updated

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #Implementación de sDES en Python.
+import fileinput as fp
 
+#Recuperando las entradas en una lista.
+inputs = []
+
+for entrada in fp.input():
+    inputs.append(entrada.strip())
+
+print(inputs)
